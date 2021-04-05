2f2578fe83ce5254cf8b2b587229d8623709b5b8
Liudas <liudvikas.griskus@ktu.edu>
2021-04-05 18:42:48 +0300

    message

---
+++ Run/app/src/main/java/com/first/run/afterLogin.kt
@@ -0,0 +1,38 @@
+package com.first.run
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class afterLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_afterlogin)
+        val signOutButton = findViewById<Button>(R.id.signout)
+        val backButton = findViewById<Button>(R.id.back)
+        val continueButton = findViewById<Button>(R.id.Continue)
+
+        signOutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+
+        backButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        continueButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
