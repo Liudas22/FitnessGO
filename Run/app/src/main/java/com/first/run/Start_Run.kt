a1f7ada4837b05c56fcd06d962cf0d37131e9654
Varnas <arnas.kvederavicius@ktu.edu>
2021-04-05 17:56:14 +0300

    Start button

---
+++ Run/app/src/main/java/com/first/run/Start_Run.kt
@@ -0,0 +1,23 @@
+package com.first.run
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.view.View
+import android.widget.EditText
+import android.content.Intent
+
+class Start_Run : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start__run)
+
+        val startButton = findViewById<Button>(R.id.Start_Button)
+        startButton.setOnClickListener{
+            val intent = Intent(this, RunFrame::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
